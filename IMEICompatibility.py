89d388d5f84865105bc5d835108629a17d4651e5
bandicoutts <14368864+bandicoutts@users.noreply.github.com>
2019-07-04 16:28:32 +1200

    Add files via upload

---
+++ IMEICompatibility.py
@@ -0,0 +1,28 @@
+import csv
+import os
+
+old_dict = {}
+new_dict = {}
+filtered_dict = {}
+old_file = "" # replace this with the directory and file name of the original file
+new_file = "" # replace this with the directory and file name of the new file
+output_file = "" # replace this with the directory and file name of the file you would like the results to be returned to
+
+with open(old_file) as f:
+  reader = csv.reader(f)
+  for row in reader:
+    old_dict[row[0]] = row[1], row[2], row[3]
+
+with open(new_file) as m:
+  reader = csv.reader(m)
+  for row in reader:
+    new_dict[row[0]] = row[1], row[2], row[3]
+
+for key,value in new_dict.iteritems():
+	if key not in old_dict:
+		filtered_dict[key] = value[0], (1 if value[1] == "TRUE" else 0), (1 if value[2] == "TRUE" else 0)
+
+with open('newfile.csv', 'wb') as f: 
+    f.write("%s,%s,%s,%s\n"%("TacCode","MktModel","DeviceBandW850","DeviceBandW2100"))
+    for key, value in filtered_dict.iteritems():
+        f.write("%s,%s,%s,%s\n"%(key,value[0], value[1], value[2]))
